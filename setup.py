21899b2869f5657405da5a4c08c829ed3e939b80
Julius Seporaitis <julius@yplanapp.com>
2016-07-08 12:31:15 +0100

    - Change version number in a separate commit.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open('LICENSE') as f:
 
 setup(
     name='salesforce-oauth-request',
-    version='1.0.6',
+    version='1.1.0',
     description='Util package to drive Salesforce Oauth Web flow for testing.',
     long_description=readme,
     author='Scott Persinger',
